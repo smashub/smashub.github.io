7b0de274e3bbef74812fc6f40f7cc599225eff6c
Jacopo de Berardinis <jonnybluesman@gmail.com>
2022-09-14 21:25:53 +0100

    gh actions for deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
   return (
     <Layout
       title={`Welcome to Smashub`}
-      description="Description will go into a meta tag in <head />">
+      description="The Semantic Music Hub">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
