86e3b11ee7226bd64fd92814ffe1fa2887fbc437
Jacopo de Berardinis <jonnybluesman@gmail.com>
2022-09-14 08:54:37 +0100

    gh-pages deployment branch update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'smashub', // Usually your GitHub org/user name.
   projectName: 'smashub.github.io', // Usually your repo name.
-  deploymentBranch: 'main',
+  deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
